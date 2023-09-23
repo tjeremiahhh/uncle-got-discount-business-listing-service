f9756521a6d1254a876b0d35711cbcc56f53701f
Jeremiah Tan <axel.max97@gmail.com>
2023-09-23 14:55:18 +0800

    Add to remote GitHub

---
+++ src/main/java/com/example/businesslistingservice/BusinessListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.businesslistingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessListingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/businesslistingservice/BusinessListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businesslistingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
