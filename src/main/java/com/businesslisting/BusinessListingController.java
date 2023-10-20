d110dc3988204280410a77414a0be90986a728c0
tjeremiahhh <axel.max97@gmail.com>
2023-10-20 15:32:16 +0800

    Improve : cors

--- src/main/java/com/businesslisting/BusinessListingController.java
+++ src/main/java/com/businesslisting/BusinessListingController.java
@@ -3,6 +3,7 @@ package com.businesslisting;
 import java.util.List;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/business-listing/")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class BusinessListingController {
 
     private final BusinessListingService businessListingService;
